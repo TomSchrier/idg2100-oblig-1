3a063efd340e8725eb5b441c4040ade1af87fcef
Tom Schrier <tomschr@stud.ntnu.no>
2021-02-07 15:02:31 +0100

    Separate JSON script from HTML and put it in its file.

---
+++ getJSON.js
@@ -0,0 +1,15 @@
+function init() {
+    fetch('coffee.json')
+        .then(response => response.json())
+        .then(JSONdata => {
+            let components = document.getElementsByTagName("coffee-status");
+
+            for (let index = 0; index < JSONdata.length; index++) {
+                if (components[index]) {
+                    components[index].setValues(JSONdata[index].level, JSONdata[index].preparedAt, JSONdata[index].temperature);
+                } else {
+                    break;
+                };
+            };
+        });
+};
